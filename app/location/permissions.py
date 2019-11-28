899ce8aa243b30962e0b9fb90511507459f620b8
Phuree Kanusont <60070075@kmitl.ac.th>
2019-11-28 07:06:07 +0700

    add comment of  auth

--- app/location/permissions.py
+++ app/location/permissions.py
@@ -1,5 +1,11 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+import requests
 
 class DockterOrReadOnly(BasePermission):
     def has_permission(self, request, view):
+        # if request.method in SAFE_METHODS:
+        #     return True
+        # if 'Authorization' in request.META.keys():
+        #     info = requests.get('http://35.236.184.82:8888/api/v1.0/me', headers={'Authorization': request.META[]})
+        # print(info.text)
         return request.method in SAFE_METHODS
